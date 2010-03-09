5bf0b4274e853e95cf8aedd4a1292d47741c0a6e
Christian Fowler <spider@viovio.com>
2010-03-09 21:25:06 +0000

    add spamurai to all content

--- Spamurai.php
+++ Spamurai.php
@@ -8,17 +8,25 @@ require_once (SPAMURAI_PKG_PATH.'Akismet.class.php');
 //TODO: getList from spamurai_log
 
 function spamurai_content_verify($pObject, $pParamHash){
-	
 	global $gBitUser, $gBitSystem;	
-	if( $gBitSystem->isPackageActive( 'spamurai' ) && is_a($pObject,'LibertyComment') ){ //Use of is_a represents a fundamental problem in the GUID system of liberty at this moment. Personally unqualified to fix, but urge others to.
-
+	if( $gBitSystem->isPackageActive( 'spamurai' ) && is_a($pObject,'LibertyContent') ){ 
 		$akismet = new Akismet( BOARDS_PKG_URI , $gBitSystem->getConfig('spamurai_api_key') );
 
 		if( !empty($pParamHash) && !empty($akismet) ) {
 			$userInfo = $gBitUser->getUserInfo( array ('user_id' => $pParamHash['user_id'] ) );	
-			$akismet->setCommentAuthor($userInfo['real_name']);
+			$akismet->setCommentAuthor( $userInfo['real_name'].$userInfo['login'] );
 			$akismet->setCommentAuthorEmail($userInfo['email']);
-			$akismet->setCommentContent($pParamHash['comment_data']);
+			$checkString = '';
+			if( !empty( $pParamHash['title'] ) ) {
+				$checkString .= $pParamHash['title'];
+			}
+			if( !empty( $pParamHash['edit'] ) ) {
+				$checkString .= $pParamHash['edit'];
+			}
+			if( !empty( $pParamHash['comment_data'] ) ) {
+				$checkString .= $pParamHash['comment_data'];
+			}
+			$akismet->setCommentContent( $checkString );
 			if($akismet->isCommentSpam()){
 				$insertSql = "INSERT INTO ".BIT_DB_PREFIX."spamurai_log (user_id, email, subject, data, posted_date) VALUES ( ?, ?, ?, ?, ? )";
 				$bindVars = array ( $pParamHash['user_id'], $userInfo['email'], !empty($pParamHash['comment_title'])?substr($pParamHash['comment_title'],0,255):'', $pParamHash['comment_data'], time() );
@@ -28,13 +36,3 @@ function spamurai_content_verify($pObject, $pParamHash){
 	}
 }
 
-
-
-
-
-
-
-
-
-
-?>
